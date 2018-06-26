9ebfed319bc7d461f4421ed070d26fd45df97768
imbilal <imadbilal21@hotmail.fr>
2018-06-26 19:16:17 +0200

    jeu quasi fini

--- visuel.h
+++ visuel.h
@@ -6,15 +6,15 @@
 #include <SDL2/SDL_ttf.h>
 #include "structures.h"
 
-typedef enum {JEU, NIVEAUX}etat_fenetre_t;
+typedef enum {JEU, NIVEAUX, VICTOIRE}etat_fenetre_t;
 
-void remplirFenetreN(SDL_Renderer **prenderer, TTF_Font *font);
+void remplirFenetreN(TTF_Font *font);
 void fenetre(SDL_Window ** pwindow, int x, int y);
 void principal(jeu_t * jeu);
-void plateau(SDL_Renderer ** prenderer, jeu_t * jeu);
-void affichefig (SDL_Renderer ** prenderer, SDL_Surface *figurine, int x, int y, int w, int h);
-int cliqueSourisN(int x, int y, jeu_t * jeu, SDL_Window ** pwindow, SDL_Renderer ** prenderer, SDL_Window ** pwindow2, SDL_Renderer ** prenderer2);
-void cliquePlateau(int x, int y, jeu_t * jeu, SDL_Window ** pwindow, SDL_Renderer ** prenderer, SDL_Window ** pwindow2, SDL_Renderer ** prenderer2, TTF_Font * font, int niveau);
-void boutons(SDL_Renderer ** prenderer);
+void plateau(jeu_t * jeu);
+void affichefig (SDL_Surface *figurine, int x, int y, int w, int h);
+int cliqueSourisN(int x, int y, jeu_t * jeu);
+void cliquePlateau(int x, int y, jeu_t * jeu, TTF_Font * font, int niveau);
+void boutons();
 
 #endif
