58fea912e35414550676bfe0acd451179c885f56
nibilly <40493957+nibilly@users.noreply.github.com>
2018-06-28 02:26:49 +0200

    Commentaires

--- visuel.c
+++ visuel.c
@@ -4,30 +4,38 @@
 #include "initialisation.h"
 #include "moteur.h"
 
-
+/*permet de savoir sur quelle fenetre nous nous trouvons*/
 etat_fenetre_t etatFenetre;
+/*identifie un element qu on va peut etre deplacer au prochain clique*/
 int sourceExiste;
 objet_t objetSource;
 int numeroSource;
+/*les fenetres*/
 SDL_Window   * window;
 SDL_Window   * window2;
 SDL_Window   * window3;
+/*les renderers*/
 SDL_Renderer * renderer;
 SDL_Renderer * renderer2;
 SDL_Renderer * renderer3;
 
+/*
+Remplit la premiere fenetre : celle des niveaux
+Avec les niveaux de 1 a 60 et des couleurs qui changent en fonction de la difficulté
+Entree : font : la police utilisee pour ecrire
+*/
 void remplirFenetreN(TTF_Font *font)
 {
 	SDL_Rect *rect;
-	int iW, iH, i, j; /* fghdsghf*/
+	int iW, iH, i, j;
 	int r, v, b;
-	SDL_Color     couleur  = {0, 0, 0, 255};
+	SDL_Color     couleur  = {0, 0, 0, 255}; /*couleur d ecriture*/
 	SDL_Surface * surf;
 	SDL_Texture * texture;
-	char num[5];
-	int numero;
+	char num[5]; /*pour le texte*/
+	int numero; /*pour la difficulté*/
 	
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);/*le font blanc*/
 	rect = malloc(sizeof(SDL_Rect));
 	rect->x = 0;
 	rect->y = 0;
@@ -48,9 +56,9 @@ void remplirFenetreN(TTF_Font *font)
 	SDL_RenderCopy(renderer, texture, NULL, rect);
 	free(rect);
 	
-	for(i =0; i<6; i++)
+	for(i =0; i<6; i/*6 lignes de dizaines*/
 	{
-		for(j=1; j<11; j++)
+		for(j=1; j<11; j++)/*10 colonnes de unites : de 1 a 10*/
 		{
 			SDL_DestroyTexture(texture);
 			SDL_FreeSurface(surf);
@@ -96,8 +104,8 @@ void remplirFenetreN(TTF_Font *font)
 			}
 			SDL_SetRenderDrawColor(renderer, r, v, b, 255);
 
-			rect->x = 30*j+30;
-			rect->y = 30*(i+1)+30;
+			rect->x = 30*j+30;/*absisses decalees de 30 + 30 pour faire une marge*/
+			rect->y = 30*(i+1)+30;/*ordonnees : pareil mais on evite le 0*/
 			rect->w = iW;
 			rect->h = iH;
 			SDL_RenderFillRect(renderer, rect);
@@ -110,6 +118,12 @@ void remplirFenetreN(TTF_Font *font)
 	SDL_DestroyTexture(texture);
 }
 
+/*
+Creer une fenetre et essaie de mettre le focus de la souris dessus
+Entrees : pwindow : pointeur de pointeur sur une fenetre
+	x : largeur
+	y : hauteur
+*/
 void fenetre(SDL_Window ** pwindow, int x, int y)
 {
 	*pwindow = SDL_CreateWindow("Lièvres et Renards", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, x, y, 0);
@@ -120,7 +134,14 @@ void fenetre(SDL_Window ** pwindow, int x, int y)
 	SDL_SetWindowInputFocus(*pwindow);
 }
 
-/*retourne le niveau*/
+/*
+Si on clique dans la zone des niveaux on enleve autant de fois qu on peut la taille d'une zone de clique reservee a un niveau
+on ferme la fenetre des niveaux et ouvre la fenetre du jeu avec le bon niveau
+Entree : x : absisse du clique en pixel
+	y : ordonnee du clique en pixel
+	jeu : la structure du jeu deja declaree
+Sortie : niveau : le niveau choisi
+*/
 int cliqueSourisN(int x, int y, jeu_t * jeu)
 {
 	int niveau = -1, dizaine=0, unite=0;
@@ -128,7 +149,7 @@ int cliqueSourisN(int x, int y, jeu_t * jeu)
 	{
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
-		x-=30;
+		x-=30;/*marge*/
 		while(x>29)
 		{
 			unite++;
@@ -145,12 +166,19 @@ int cliqueSourisN(int x, int y, jeu_t * jeu)
 		
 		etatFenetre = JEU;
 		fenetre(&window2, 500, 600);
-	    renderer2 = SDL_CreateRenderer(window2, -1, SDL_RENDERER_ACCELERATED);
-		plateau(jeu);
+	    	renderer2 = SDL_CreateRenderer(window2, -1, SDL_RENDERER_ACCELERATED);
+		plateau(jeu);/*remplissage*/
 	}
 	return niveau;
 }
 
+/*
+Si estGagne a la fin d'un deplacement
+Ferme la fenetre du plateau, affiche une nouvelle fenetre avec une image Gagne! 
+et le nombre de deplacement par rapport au nombre minimum possible
+Entree : font : la meme police d ecriture que la fenetre des niveaux
+	jeu : structure du jeu pour les nbDeplacements et nbCoutsMin
+*/
 void victoire(TTF_Font * font, jeu_t * jeu)
 {
 	int iW, iH;
@@ -183,7 +211,7 @@ void victoire(TTF_Font * font, jeu_t * jeu)
 	SDL_RenderCopy(renderer3, avatar, NULL, rect);
 	free(rect);
 	
-	SDL_SetRenderDrawColor(renderer3, 255, 255, 255, 0);
+	SDL_SetRenderDrawColor(renderer3, 255, 255, 255, 0);/*Petite zone blanche pour faire jolie*/
 	rect = malloc(sizeof(SDL_Rect));
 	rect->x = 0;
 	rect->y = 400;
@@ -213,10 +241,14 @@ void victoire(TTF_Font * font, jeu_t * jeu)
 	SDL_DestroyTexture(texture);
 }
 
+/*
+Affichage du plateau de jeu
+d abord le plateau en font puis on parcours jeu->plateua qui est une matrice correspondant a l affichage de la partie
+pour chaque case de jeu->plateau si on identifit un element de taille 100*100 alors on l'ajoute sur la fenetre
+Entree : 
+*/
 void plateau(jeu_t * jeu)
 {
-/*initialisation*/
-	
 	int ligne, colonne;
 	int cmpt0 = 0;
 	int cmpt1= 0;
@@ -287,10 +319,13 @@ void plateau(jeu_t * jeu)
 			}
 		}
 	}
-	boutons();
+	boutons();/*On affiche deux zones de textes en haut du plateau*/
 	SDL_RenderPresent(renderer2);
 }
 
+/*
+Les boutons Quitter et Initialiser sont de simples textes blancs sur font noir pour plus de discretion
+*/
 void boutons()
 {
 	SDL_Rect *rect;
@@ -298,7 +333,7 @@ void boutons()
 	SDL_Color     couleur  = {255, 255, 255, 255};
 	SDL_Surface * surf;
 	SDL_Texture * texture;
-	TTF_Font * font2 = TTF_OpenFont("fake.receipt.ttf", 35);
+	TTF_Font * font2 = TTF_OpenFont("fake.receipt.ttf", 35);/*une police plus grosse cette fois ci*/
 	
 	surf     = TTF_RenderText_Blended(font2, "Quitter", couleur);
 	texture = SDL_CreateTextureFromSurface(renderer2, surf);
@@ -330,7 +365,16 @@ void boutons()
 	
 	TTF_CloseFont(font2);
 }
-
+	    
+/*
+utilisee dans plateau()
+integre une surface a la fenetre du plateau a un certain endroit
+Limite la redondance de code
+Entree : pfigurine : pointeur de pointeur sur une figure/image dans notre cas
+	x, y : absisse et ordonnee du debut de l image
+	w : largeur de l image
+	h : hauteur de l image
+*/
 void affichefig (SDL_Surface **pfigurine, int x, int y, int w, int h)
 {
 	SDL_Rect *rect;
@@ -349,13 +393,24 @@ void affichefig (SDL_Surface **pfigurine, int x, int y, int w, int h)
 	SDL_FreeSurface(*pfigurine);
 }
 
+/*
+Lors d'un clique sur la fenetre du jeu on a deux choit : soit on est dans la zone des boutons au dessus soit on est sur le plateau
+Si on veut quitter alors on ferme la fenetre et on ouvre la fenetre des niveaux
+Si on veut recommencer alors on relance initialiserNiveau() sur le jeu liberer et declarer et on met a jour l affichage
+Sinon on identifie sur quelle case on a clique et si on a une source qui existe(precedent clique etait sur un element) on essai de le deplacer sur le nouveau clique
+						Sinon on doit identifier la source : objet et numero
+	et on regarde si on a gagne
+Entree : x, y : absisse et ordonnee du clique
+	jeu : le jeu en cours
+	font : la police en cas de victoire ou de retour au choix du niveau
+*/
 void cliquePlateau(int x, int y, jeu_t * jeu, TTF_Font * font, int niveau)
 {
 	int ligne = 0, colonne = 0;
 	char elementPlateau[5];
 	if(y<100)
 	{
-		liberer(jeu);
+		liberer(jeu);/*va tout changer dans les deux cas*/
 		jeu = declarerJeu();
 		if(x<225)
 		{
@@ -438,6 +493,15 @@ void cliquePlateau(int x, int y, jeu_t * jeu, TTF_Font * font, int niveau)
 	
 }
 
+/*
+Initialise les librairies SDL utilisees
+Ouvre la fenetre des niveaux
+Tant qu'on n'arrete pas on regarde si il y a un evenement toutes les 10ms se qui laisse le temps a l'affichage de se faire correctement
+Gere le clique sur la croix, le quitte et surtout le clique. Ici intervient etatFenetre car la fonction appelee differe.
+Pour le clque sur la fenetre de victoire on la ferme et on ouvre sur la fenetre des niveaux
+A la sorie de la boucle on ferme la fenetre en cours, on ferme les librairies et libere le jeu si necessaire.
+Entree : jeu : la structure jeu allouee
+*/
 void principal(jeu_t * jeu)
 {
 	int niveau;
@@ -460,7 +524,7 @@ void principal(jeu_t * jeu)
 	font = TTF_OpenFont("fake.receipt.ttf", 15);
 	etatFenetre = NIVEAUX;
 	fenetre(&window, 390, 270);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
+    	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
 	remplirFenetreN(font);
 	if (renderer == 0) {
 		 fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
@@ -539,7 +603,7 @@ void principal(jeu_t * jeu)
 	}
 	TTF_Quit();
 	SDL_Quit();
-	if(jeu != NULL)/*depend de si on a déjà joué ou non*/
+	if(jeu != NULL)/*depend de si on a deja joue ou non*/
 	{
 		liberer(jeu);
 	}
